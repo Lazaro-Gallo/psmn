0c72ce3b09df8161a3a42f1ac73736ba95ebe7f7
wlamirl <wlamirl@yahoo.com.br>
2016-02-10 14:40:27 -0200

    Wlamir - Tabelas de emails 2016

---
+++ htdocs/docs/StateManagerEmail.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.12
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 10, 2016 at 02:38 PM
+-- Server version: 5.1.73
+-- PHP Version: 5.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `PSMN_Prd`
+--
+
+--
+-- Dumping data for table `StateManagerEmail`
+--
+
+INSERT INTO bd_psmn_hmg.StateManagerEmail (Id, StateId, Email) VALUES
+(1, 1, 'maria@ac.sebrae.com.br'),
+(2, 2, 'pollyanna.melo@al.sebrae.com.br'),
+(3, 3, 'osiana.nogueira@am.sebrae.com.br'),
+(4, 4, 'gilvana@ap.sebrae.com.br'),
+(5, 5, 'suely.paula@ba.sebrae.com.br'),
+(6, 6, 'christine.satiro@ce.sebrae.com.br'),
+(7, 7, 'fabiane.crispim@df.sebrae.com.br'),
+(8, 8, 'marceliy.bridi@es.sebrae.com.br'),
+(9, 9, 'lucia@sebraego.com.br'),
+(10, 10, 'amparo@ma.sebrae.com.br'),
+(14, 14, 'renata@pa.sebrae.com.br'),
+(15, 15, 'mjmp@pb.sebrae.com.br'),
+(17, 17, 'luzinete@pi.sebrae.com.br'),
+(18, 18, 'lhahn@pr.sebrae.com.br'),
+(19, 19, 'eoliveira@rj.sebrae.com.br'),
+(20, 20, 'etelvina@rn.sebrae.com.br'),
+(21, 21, 'cristiano.rodrigues@ro.sebrae.com.br'),
+(22, 22, 'eliene.araujo@rr.sebrae.com.br'),
+(23, 23, 'roseli@sebrae-rs.com.br'),
+(24, 24, 'vitarelli@sc.sebrae.com.br'),
+(25, 25, 'aldeci.andrade@se.sebrae.com.br'),
+(26, 26, 'anamariab@sp.sebrae.com.br'),
+(27, 27, 'barbara.nunes@to.sebrae.com.br'),
+(28, 21, 'inacia@ro.sebrae.com.br'),
+(29, 13, 'liliane.moreira@mt.sebrae.com.br'),
+(30, 11, 'luciana.lessa@sebraemg.com.br'),
+(31, 16, 'lucianacorreia@pe.sebrae.com.br'),
+(32, 12, 'lucielle.lima@ms.sebrae.com.br');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ htdocs/docs/whitelist.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.12
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 10, 2016 at 02:35 PM
+-- Server version: 5.1.73
+-- PHP Version: 5.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `PSMN_Prd`
+--
+
+--
+-- Dumping data for table `Whitelist`
+--
+
+INSERT INTO bd_psmn_hmg.Whitelist (Id, Context, Value) VALUES
+(1, 'email', 'aldeci.andrade@se.sebrae.com.br'),
+(2, 'email', 'amparo@ma.sebrae.com.br'),
+(3, 'email', 'anamariab@sp.sebrae.com.br'),
+(4, 'email', 'barbara.nunes@to.sebrae.com.br'),
+(5, 'email', 'christine.satiro@ce.sebrae.com.br'),
+(6, 'email', 'eliene.araujo@rr.sebrae.com.br'),
+(7, 'email', 'eoliveira@sebraerj.com.br'),
+(8, 'email', 'etelvina@rn.sebrae.com.br'),
+(9, 'email', 'fabiane.crispim@df.sebrae.com.br'),
+(10, 'email', 'gilvana@ap.sebrae.com.br'),
+(11, 'email', 'inacia@ro.sebrae.com.br'),
+(12, 'email', 'ihahn@pr.sebrae.com.br'),
+(13, 'email', 'liliane.moreira@mt.sebrae.com.br'),
+(14, 'email', 'lucia@sebraego.com.br'),
+(15, 'email', 'luciana.lessa@sebraemg.com.br'),
+(16, 'email', 'lucianacorreia@pe.sebrae.com.br'),
+(17, 'email', 'lucielle.lima@ms.sebrae.com.br'),
+(18, 'email', 'luzinete@pi.sebrae.com.br'),
+(19, 'email', 'marceliy.bridi@es.sebrae.com.br'),
+(20, 'email', 'maria@ac.sebrae.com.br'),
+(21, 'email', 'mjmp@pb.sebrae.com.br'),
+(22, 'email', 'osiana.nogueira@am.sebrae.com.br'),
+(23, 'email', 'pollyanna.melo@al.sebrae.com.br'),
+(24, 'email', 'renata@pa.sebrae.com.br'),
+(25, 'email', 'roseli@sebrae-rs.com.br'),
+(26, 'email', 'sueli.paula@ba.sebrae.com.br'),
+(27, 'email', 'vitarelli@sc.sebrae.com.br');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
